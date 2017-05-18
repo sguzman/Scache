7d132a7b63a03ab4b2567db49e9bc294bb0397bb
Salvador Guzman <guzmansalv@gmail.com>
2017-05-18 10:35:13 -0700

    set up proxy server with permanence caching mechanism

--- src/main/scala/com/github/sguzman/scache/Scache.scala
+++ src/main/scala/com/github/sguzman/scache/Scache.scala
@@ -1,9 +1,11 @@
 package com.github.sguzman.scache
 
+import java.io.{File, FileOutputStream, PrintWriter}
 import java.util.concurrent.Executors
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.server.{HttpApp, Route}
@@ -12,8 +14,10 @@ import akka.util.ByteString
 import org.apache.commons.lang3.StringUtils
 import org.pmw.tinylog.Logger
 
+import scala.collection.mutable
 import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutorService}
 import scala.concurrent.duration.Duration
+import scala.io.Source
 
 // Server definition
 object Scache extends HttpApp {
@@ -21,29 +25,59 @@ object Scache extends HttpApp {
   implicit val materializer = ActorMaterializer()
   implicit val executor: ExecutionContextExecutorService = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))
 
-
+  val filename = "./src/main/res/data.db"
+  var cache: Map[String, String] = this.getCache
   var reqCounter: Long = 0
 
   def route: Route =  {
     extractUri { uri =>
       entity(as[String]) { body =>
-        val path = uri.path.toString
-        val qs = Map[String,String](uri.rawQueryString.getOrElse("").split('&').map(_.split('=')).map(s => (s(0), s(1))): _*)
-        val host = qs.getOrElse("gyg-host", "")
-        val querystring = qs.getOrElse("gyg-querystring", "")
-        val rawHeaders = qs.filterKeys(_.startsWith("gyg-header-")).map({case (k,v) => RawHeader(StringUtils.substringAfter(k, "gyg-header-").capitalize, v)}).toList
+        if (cache.contains(uri.toString)) {
+          complete(HttpResponse(entity = HttpEntity(cache.getOrElse(uri.toString, ""))))
+        } else {
+          val path = uri.path.toString
+          val qs = Map[String,String](uri.rawQueryString.getOrElse("").split('&').map(_.split('=')).map(s => (s(0), s(1))): _*)
+          val host = qs.getOrElse("gyg-host", "")
+          val querystring = qs.getOrElse("gyg-querystring", "")
+          val rawHeaders = qs.filterKeys(_.startsWith("gyg-header-")).map({case (k,v) => RawHeader(StringUtils.substringAfter(k, "gyg-header-").capitalize, v)}).toList
 
-        val httpPrep = HttpRequest(HttpMethods.GET, s"https://$host$path?$querystring").withEntity(body)
-        rawHeaders.foreach(httpPrep.withHeaders(_))
+          val httpPrep = HttpRequest(HttpMethods.GET, s"https://$host$path?$querystring").withEntity(body)
+          rawHeaders.foreach(httpPrep.withHeaders(_))
 
-        val result = Await.result(Http().singleRequest(httpPrep), Duration.Inf)
-        this.metaPrint(host, path, querystring, body, rawHeaders, result)
-        complete(result)
+          val result = Await.result(Http().singleRequest(httpPrep), Duration.Inf)
+          val entityBody = Await.result(result.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(_.utf8String), Duration.Inf)
+          this.metaPrint(host, path, querystring, body, rawHeaders, result, entityBody)
+
+          if (result.status.isSuccess) {
+            cache += (uri.toString -> entityBody)
+
+            this.setCache()
+          }
+
+          complete(result)
+        }
       }
     }
   }
 
-  def metaPrint(host: String, path: String, qs: String, body: String, headers: List[RawHeader], resp: HttpResponse): Unit = {
+  def getCache: Map[String,String] = {
+    new FileOutputStream(this.filename, true).close()
+    val fileInnards = Source.fromFile(this.filename).mkString
+    if (fileInnards.isEmpty) {
+      return Map[String,String]()
+    }
+
+    upickle.default.read[Map[String,String]](fileInnards)
+  }
+
+  def setCache(): Unit = {
+    new PrintWriter(filename) {
+      write(upickle.default.write[Map[String,String]](Scache.cache))
+      close()
+    }
+  }
+
+  def metaPrint(host: String, path: String, qs: String, body: String, headers: List[RawHeader], resp: HttpResponse, entityBody: String): Unit = {
     Logger.info(
       s"""
          |\t PROXY REQUEST - [${this.reqCounter}]
@@ -67,12 +101,12 @@ object Scache extends HttpApp {
          |\t\tContent Type
          |\t\t\t${resp.entity.contentType.toString}
          |\t\tResponse Body
-         |\t\t\t${Await.result(resp.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(_.utf8String), Duration.Inf)}
+         |\t\t\t$entityBody
          |\t PROXY REQUEST - [${this.reqCounter}]
        """.stripMargin
     )
     this.reqCounter += 1
   }
 
-  def main(args: Array[String]): Unit = this.startServer("localhost", 8080)
+  def main(args: Array[String]): Unit = this.startServer("localhost", 8000)
 }
