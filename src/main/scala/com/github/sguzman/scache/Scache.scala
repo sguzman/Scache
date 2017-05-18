0b8e8e9d3091ec6ef52ed977e65a0ac808858f22
Salvador Guzman <guzmansalv@gmail.com>
2017-05-18 07:51:34 -0700

    finished proxy mechanism

--- src/main/scala/com/github/sguzman/scache/Scache.scala
+++ src/main/scala/com/github/sguzman/scache/Scache.scala
@@ -1,65 +1,78 @@
 package com.github.sguzman.scache
 
-import java.net.InetSocketAddress
-import com.sun.net.httpserver.{HttpExchange, HttpHandler, HttpServer}
-import scala.language.postfixOps
+import java.util.concurrent.Executors
 
-object Scache {
-  def main(args: Array[String]) {
-    val server = HttpServer.create(new InetSocketAddress(8000), 0)
-    server.createContext("/", new Scache())
-    server.setExecutor(null)
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.server.{HttpApp, Route}
+import akka.stream.ActorMaterializer
+import akka.util.ByteString
+import org.apache.commons.lang3.StringUtils
+import org.pmw.tinylog.Logger
 
-    server.start()
-    println("Hit Q key to exit...")
+import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutorService}
+import scala.concurrent.duration.Duration
 
-    while (true) {
-      if ('q' == System.in.read()) {
-        server.stop(0)
+// Server definition
+object Scache extends HttpApp {
+  implicit val system = ActorSystem("system")
+  implicit val materializer = ActorMaterializer()
+  implicit val executor: ExecutionContextExecutorService = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))
+
+
+  var reqCounter: Long = 0
+
+  def route: Route =  {
+    extractUri { uri =>
+      entity(as[String]) { body =>
+        val path = uri.path.toString
+        val qs = Map[String,String](uri.rawQueryString.getOrElse("").split('&').map(_.split('=')).map(s => (s(0), s(1))): _*)
+        val host = qs.getOrElse("gyg-host", "")
+        val querystring = qs.getOrElse("gyg-querystring", "")
+        val rawHeaders = qs.filterKeys(_.startsWith("gyg-header-")).map({case (k,v) => RawHeader(StringUtils.substringAfter(k, "gyg-header-").capitalize, v)}).toList
+
+        val httpPrep = HttpRequest(HttpMethods.GET, s"https://$host$path?$querystring").withEntity(body)
+        rawHeaders.foreach(httpPrep.withHeaders(_))
+
+        val result = Await.result(Http().singleRequest(httpPrep), Duration.Inf)
+        this.metaPrint(host, path, querystring, body, rawHeaders, result)
+        complete(result)
       }
     }
-
-  }
-}
-
-class Scache extends HttpHandler{
-  var idx: Int = 0
-
-  def handle(t: HttpExchange) {
-    print(t)
-    sendResponse(t)
   }
 
-  def print(resq: HttpExchange): Unit ={
-    val query = resq.getRequestURI.getQuery
-    val path = resq.getRequestURI.getPath
-    val headers = resq.getRequestHeaders
-    val host = if (headers.containsKey("gyg-host")) headers.get("gyg-host") else ""
-    println
-    println(s"******************** REQUEST START: [${this.idx}] ********************")
-    println
-    println("\tRequest Data")
-    println(s"\t\tPath")
-    println(s"\t\t\t[$path]")
-    println(s"\t\tQuery String")
-    println(s"\t\t\t[$query]")
-    println(s"\t\tHost")
-    println(s"\t\t\t[$host]")
-    println(s"\t\tHeaders")
-    println("\tResponse Body")
-    println(s"\t\tData")
-    println(s"\t\t\t[]")
-    println
-    println(s"********************* REQUEST END: [${this.idx}] *********************")
-    println
-    this.idx += 1
+  def metaPrint(host: String, path: String, qs: String, body: String, headers: List[RawHeader], resp: HttpResponse): Unit = {
+    Logger.info(
+      s"""
+         |\t PROXY REQUEST - [${this.reqCounter}]
+         |\t\tHost
+         |\t\t\t$host
+         |\t\tPath
+         |\t\t\t$path
+         |\t\tQuery String
+         |\t\t\t$qs
+         |\t\tBody
+         |\t\t\t$body
+         |\t\tHeaders
+         |\t\t\t${headers.mkString("\n")}
+         |
+         |\t---------------------------
+         |
+         |\t\tStatus Code
+         |\t\t\t${resp.status}
+         |\t\tContent Length
+         |\t\t\t${resp.entity.contentLengthOption.getOrElse(-1)}
+         |\t\tContent Type
+         |\t\t\t${resp.entity.contentType.toString}
+         |\t\tResponse Body
+         |\t\t\t${Await.result(resp.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(_.utf8String), Duration.Inf)}
+         |\t PROXY REQUEST - [${this.reqCounter}]
+       """.stripMargin
+    )
+    this.reqCounter += 1
   }
 
-  private def sendResponse(t: HttpExchange) {
-    val response = "Ack!"
-    t.sendResponseHeaders(200, response.length())
-    val os = t.getResponseBody
-    os.write(response.getBytes)
-    os.close()
-  }
+  def main(args: Array[String]): Unit = this.startServer("localhost", 8080)
 }
