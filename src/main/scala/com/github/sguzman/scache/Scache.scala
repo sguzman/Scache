a03f41960ac327c691b13e3a8be6b3a19f10ad31
Salvador Guzman <guzmansalv@gmail.com>
2017-05-21 10:12:44 -0700

    put headers in Content-Language and parse out as JSON use CorsDirective.cors to handle cors issue only cache when getting 200 status code

--- src/main/scala/com/github/sguzman/scache/Scache.scala
+++ src/main/scala/com/github/sguzman/scache/Scache.scala
@@ -6,11 +6,13 @@ import java.util.concurrent.Executors
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.model.headers.{HttpOriginRange, RawHeader}
 import akka.http.scaladsl.server.{HttpApp, Route}
 import akka.stream.ActorMaterializer
 import akka.util.ByteString
-import org.apache.commons.lang3.StringUtils
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives
+import ch.megard.akka.http.cors.scaladsl.settings.CorsSettings
+import org.json.{JSONObject, JSONTokener}
 import org.pmw.tinylog.Logger
 
 import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutorService}
@@ -26,37 +28,42 @@ object Scache extends HttpApp {
   var cache: Map[String, String] = this.getCache
   var reqCounter: Long = 0
 
+  var settings = CorsSettings.defaultSettings.copy(allowGenericHttpRequests = true, allowCredentials = false, allowedOrigins = HttpOriginRange.*)
+
   def route: Route =  {
-    extractRequest { request =>
-      val headers = Map[String, String](request.headers.filter(_.name().startsWith("Gyg-")).map(h => (StringUtils.substringAfter(h.name(), "Gyg-").capitalize, h.value())): _*)
-      val body = Await.result(request.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(_.utf8String), Duration.Inf)
-      val key = s"${request.uri.toString}${headers.mkString}$body"
+    CorsDirectives.cors(settings) {
+      extractRequest { request =>
+        val content = request.headers.filter(_.name == "Content-Language").head.value
+        val headers = new JSONObject(new JSONTokener(content))
+        val body = Await.result(request.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(_.utf8String), Duration.Inf)
+        val key = s"${request.uri.toString}$content$body"
 
-      if (cache.contains(key)) {
-        complete(HttpResponse(entity = HttpEntity(cache.getOrElse(key, ""))))
-      } else {
+        if (cache.contains(key)) {
+          complete(HttpResponse(entity = HttpEntity(cache.getOrElse(key, ""))))
+        } else {
 
-        val path = request.uri.path.toString
-        var querystring = request.uri.rawQueryString.getOrElse("")
-        if (!querystring.isEmpty) {
-          querystring = "?" + querystring
+          val path = request.uri.path.toString
+          var querystring = request.uri.rawQueryString.getOrElse("")
+          if (!querystring.isEmpty) {
+            querystring = "?" + querystring
+          }
+
+          val host = headers.getString("Host")
+          val httpPrep = HttpRequest(HttpMethods.GET, s"https://$host$path$querystring", Nil, HttpEntity(body), HttpProtocols.`HTTP/1.1`)
+          headers.keySet.forEach(h => httpPrep.withHeaders(RawHeader(h, headers.getString(h))))
+
+          val result = Await.result(Http().singleRequest(httpPrep), Duration.Inf)
+          val entityBody = Await.result(result.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(_.utf8String), Duration.Inf)
+          this.metaPrint(host, path, querystring, body, content, result, entityBody)
+
+          if (result.status.intValue() == 200) {
+            cache += (key -> entityBody)
+
+            this.setCache()
+          }
+
+          complete(HttpResponse(entity = HttpEntity(entityBody)))
         }
-
-        val host = headers.getOrElse("Host", "")
-        val httpPrep = HttpRequest(HttpMethods.GET, s"https://$host$path$querystring").withEntity(body)
-        headers.foreach(h => httpPrep.withHeaders(RawHeader(h._1, h._2)))
-
-        val result = Await.result(Http().singleRequest(httpPrep), Duration.Inf)
-        val entityBody = Await.result(result.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(_.utf8String), Duration.Inf)
-        this.metaPrint(host, path, querystring, body, headers, result, entityBody)
-
-        if (result.status.isSuccess) {
-          cache += (key -> entityBody)
-
-          this.setCache()
-        }
-
-        complete(result)
       }
     }
   }
@@ -78,7 +85,7 @@ object Scache extends HttpApp {
     }
   }
 
-  def metaPrint(host: String, path: String, qs: String, body: String, headers: Map[String,String], resp: HttpResponse, entityBody: String): Unit = {
+  def metaPrint(host: String, path: String, qs: String, body: String, headers: String, resp: HttpResponse, entityBody: String): Unit = {
     Logger.info(
       s"""
          |\t PROXY REQUEST - [${this.reqCounter}]
@@ -91,7 +98,7 @@ object Scache extends HttpApp {
          |\t\tBody
          |\t\t\t$body
          |\t\tHeaders
-         |\t\t\t${headers.mkString("\n")}
+         |\t\t\t$headers
          |
          |\t---------------------------
          |
